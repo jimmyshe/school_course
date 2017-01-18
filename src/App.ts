f863101aaf2c13a292e1d1d67acce12e0e442c94
Sizhen she <cq2essz@gmail.com>
2017-01-18 17:36:51 +0000

    changed a port

--- src/App.ts
+++ src/App.ts
@@ -23,4 +23,5 @@ export class App {
 // This ends up starting the whole system and listens on a hardcoded port (4321)
 Log.info('App - starting');
 let app = new App();
-app.initServer(4321);
+//app.initServer(4321);
+app.initServer(8080); // use 8080 make debuging easier
