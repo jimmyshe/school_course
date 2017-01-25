0e02d788ef52da491f1d9ded4bd0a66cbaab7db8
JimmyShe <cq2essz@gmail.com>
2017-01-25 15:52:57 -0800

    Finished the implementation of filter

--- src/controller/queryHelper.ts
+++ src/controller/queryHelper.ts
@@ -2,6 +2,8 @@ import {IInsightFacade, InsightResponse, QueryRequest} from "./IInsightFacade";
 import {Section} from "./CourseInformation";
 import Log from "../Util";
 import {error} from "util";
+import {isNumber} from "util";
+import {isString} from "util";
 
 export default class QH {
        /*
@@ -46,39 +48,139 @@ export default class QH {
                                     'IS'?:{}
                                     'NOT'?:{}}):boolean[]{
 
-        if (filter['LT']!=null){
-           let comparision_class:any = filter['LT'];
-
-           if(Object.keys(comparision_class).length!=1){
-               throw new Error("more than one comparision key in filter")
-           }
-
-           let comparision_key:string =  Object.keys(comparision_class)[0];
-           let comparision_value:number = comparision_class[comparision_key];
-
-           let ret:boolean[] = [];
-           let len = courseInformation.length;
-           for(let i = 0;i<len;i++){
-               if((courseInformation[i] as any)[comparision_key]==null){
-                   throw new Error("Can't find comparision_key in the data set");
-               }else {
-                   if ((courseInformation[i] as any)[comparision_key] < comparision_value) {
-                       ret.push(true);
-                   }
-                   else {
-                       ret.push(false);
-                   }
-               }
-           }
-           return ret;
+        if(Object.keys(filter).length!=1){
+            throw new Error('the filter is not valid');
         }
 
-        if(filter['NOT']!=null){
-            let ret:boolean[] = QH.filterOut(courseInformation,filter['NOT']);
-            for(let i of ret){
-                i = !i;
+        let filter_key = Object.keys(filter)[0];
+
+        if(filter_key=='LT'||filter_key=='GT'||filter_key=='EQ') {
+
+            let comparision_class: any = filter[filter_key];
+            if (Object.keys(comparision_class).length != 1) {
+                throw new Error("the filter is not valid");
+            }
+
+            let comparision_key: string = Object.keys(comparision_class)[0];
+            let comparision_value = comparision_class[comparision_key];
+
+            if(!isNumber(comparision_value)){
+                throw new Error("the filter is not valid");
+            }
+
+
+            let ret: boolean[] = [];
+            let len = courseInformation.length;
+            for (let i = 0; i < len; i++) {
+                if ((courseInformation[i] as any)[comparision_key] == null) {
+                    throw new Error("Can't find " + comparision_key + " in the data set");
+                } else {
+
+                    if(!isNumber((courseInformation[i] as any)[comparision_key])){
+                        throw new Error("the filter is not valid");
+                    }
+
+                    switch (filter_key){
+                        case 'LT':
+                            if ((courseInformation[i] as any)[comparision_key] < comparision_value) {
+                                ret.push(true);
+                            }
+                            else {
+                                ret.push(false);
+                            }
+                            break;
+                        case 'EQ':
+                            if ((courseInformation[i] as any)[comparision_key] == comparision_value) {
+                                ret.push(true);
+                            }
+                            else {
+                                ret.push(false);
+                            }
+                            break;
+                        case 'GT':
+                            if ((courseInformation[i] as any)[comparision_key] > comparision_value) {
+                                ret.push(true);
+                            }
+                            else {
+                                ret.push(false);
+                            }
+                            break;
+                    }
+                }
             }
             return ret;
-        }
+        }   // this is the case of MCOMPARISON
+
+        if(filter_key=='IS'){
+
+            let comparision_class: any = filter[filter_key];
+            if (Object.keys(comparision_class).length != 1) {
+                throw new Error("the filter is not valid");
+            }
+
+            let comparision_key: string = Object.keys(comparision_class)[0];
+            let comparision_value = comparision_class[comparision_key];
+
+            if(!isString(comparision_value)){
+                throw new Error("the filter is not valid");
+            }
+
+            let ret: boolean[] = [];
+            let len = courseInformation.length;
+            for (let i = 0; i < len; i++) {
+                if ((courseInformation[i] as any)[comparision_key] == null) {
+                    throw new Error("Can't find " + comparision_key + " in the data set");
+                } else {
+
+                    if(!isString((courseInformation[i] as any)[comparision_key])){
+                        throw new Error("the filter is not valid");
+                    }
+
+                    if ((courseInformation[i] as any)[comparision_key] == comparision_value) {
+                        ret.push(true);
+                    }
+                    else {
+                        ret.push(false);
+                    }
+                }
+            }
+            return ret;
+
+        }                       // this is the case of SCOMPARISON
+
+        if(filter_key=='AND'||filter_key=='OR'){
+            let logicfilters: {}[] = filter[filter_key];
+            if (logicfilters.length < 2) {
+                throw new Error("the filter is not valid");
+            }
+
+            let pre_ret:boolean[] = QH.filterOut(courseInformation,logicfilters[0]);
+
+            for(let i=1;i<logicfilters.length;i++){
+                let cur_ret = QH.filterOut(courseInformation,logicfilters[i]);
+
+                if(filter_key=='AND'){
+                    for(let j=0;j<pre_ret.length;j++){
+                        pre_ret[j]=pre_ret[j]&&cur_ret[j];
+                    }
+                }
+                else { // filter_key = 'OR
+                    for(let j=0;j<pre_ret.length;j++){
+                        pre_ret[j]=pre_ret[j]||cur_ret[j];
+                    }
+                }
+            }
+            return pre_ret;
+        }   // This is the case for logic
+
+        if(filter_key='NOT'){
+            let ret:boolean[] = QH.filterOut(courseInformation,filter['NOT']);
+            for(let i =0;i<ret.length;i++){
+                ret[i]=!ret[i];
+            }
+            return ret;
+        }                     // this is the case for negation
+
+        throw new Error("the filter is not valid");  // the filter is not any of the for types
     }
 }
