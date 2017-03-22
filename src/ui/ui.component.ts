27eeac8d3efe5fb8caadf901ea2b915065529335
JimmyShe <cq2essz@gmail.com>
2017-03-21 21:10:24 -0700

    learn how to display info

--- src/ui/ui.component.ts
+++ src/ui/ui.component.ts
@@ -2,8 +2,40 @@
  * Created by jimmyshe-ubuntu on 17-3-21.
  */
 import { Component } from '@angular/core';
+
+
 @Component({
     selector: 'my-app',
-    template: '<h1>我的第一个 Angular 应用</h1>'
+    template: '<h2>{{result.render}}</h2>' +
+        '<p>result:</p>' +
+    '<li *ngFor="let ret of result.result">' +
+    '{{ret.rooms_name}}</li> '
 })
-export class AppComponent { }
+
+
+
+export class AppComponent {
+
+    result = {
+    "render": "TABLE",
+    "result": [
+        {
+            "rooms_name": "DMP_101"
+        },
+        {
+            "rooms_name": "DMP_110"
+        },
+        {
+            "rooms_name": "DMP_201"
+        },
+        {
+            "rooms_name": "DMP_301"
+        },
+        {
+            "rooms_name": "DMP_310"
+        }
+    ]
+    }
+
+
+}
